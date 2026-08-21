57fca1c356e5700ae5235752dd48a629195ed786
Maximo Perez <maximo.pbeltramone@gmail.com>
2026-08-21 12:09:34 -0300

    completar ejercicio 4

--- TPN1/Ejercicio4/src/main/java/com/ejercicio4/mvc/models/enums/EstadoIntento.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio4.mvc.models.enums;
-
-/**
- * Enumeración que representa el resultado de un intento de inicio de sesión.
- */
-public enum EstadoIntento {
-    INICIO_SESION_EXITOSO,
-    INICIO_SESION_FALLIDO
-}
